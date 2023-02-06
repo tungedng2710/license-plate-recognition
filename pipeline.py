ff3c11e41f10b359eac06f742a96e5ec6e24914c
tungedng2710 <tungnguyen99.tn@gmail.com>
2023-02-06 17:51:49 +0700

    fix displaying

--- pipeline.py
+++ pipeline.py
@@ -1,12 +1,36 @@
-import cv2
 import shutil
 import os
 import random
 import string
-import torch.nn as nn
+import numpy as np
+import cv2
+
+import torch
+from torch import nn
 
 from ultralytics import YOLO
-from PIL import Image
+
+os.environ["YOLOv8_VERBOSE"] = "False"
+BGR_COLORS = {
+    "blue": (255, 0, 0),
+    "green": (0, 255, 0),
+    "red": (0, 0, 255),
+    "amber": (0, 191, 255)
+}
+def check_image_size(image, w_thres, h_thres):
+    """
+    Ignore small images
+    Args: image, w_thres, h_thres
+    """
+    if w_thres is None:
+        w_thres = 64
+    if h_thres is None:
+        h_thres = 64
+    width, height, _ = image.shape
+    if (width >= w_thres) and (height >= h_thres):
+        return True
+    else:
+        return False
 
 def draw_text(img, text,
             pos=(0, 0),
@@ -16,18 +40,26 @@ def draw_text(img, text,
             text_color=(0, 0, 255),
             text_color_bg=(0, 255, 0)
             ):
+    """
+    Minor modification of cv2.putText to add background color
+    """
     x, y = pos
     text_size, _ = cv2.getTextSize(text, font, font_scale, font_thickness)
     text_w, text_h = text_size
     cv2.rectangle(img, pos, (x + text_w, y + text_h), text_color_bg, -1)
-    cv2.putText(img, text, (x, y + text_h + font_scale - 1), font, font_scale, text_color, font_thickness)
+    pos = (x, y + text_h + font_scale - 1)
+    cv2.putText(img, text, pos, font, font_scale, text_color, font_thickness)
 
 class DummyModel(nn.Module):
+    """
+    I'm just a Dummy model for filling the gap
+    Replace me with an OCR model
+    """
     def __init__(self):
         super().__init__()
-        print("This is a dummy OCR model, replace it with another one!")
+        print("You are using dummy OCR model!")
 
-    def forward(self, x):
+    def forward(self, input):
         number = random.uniform(1, 9)
         number = int(10000*number)
         number = str(number)
@@ -36,14 +68,18 @@ class DummyModel(nn.Module):
         return dummy_output
 
 class Pipeline():
+    """
+    License plate OCR pipeline
+    """
     def __init__(self,
                  source: str = "data",
                  vehicle_weight: str = None,
                  plate_weight: str = None,
                  use_sd_resolution: bool = False,
                  use_hd_resolution: bool = True,
-                 use_fhd_resolution: bool = False):
-        # Models
+                 use_fhd_resolution: bool = False,
+                 save_plate_image: bool = False):
+        # Pipeline core
         self.source = source
         self.vehicle_weight = vehicle_weight
         self.image = cv2.imread(source)
@@ -62,7 +98,23 @@ class Pipeline():
             self.use_hd_resolution = False
             self.use_fhd_resolution = False
 
+        # Miscellaneous for displaying
+        self.color = BGR_COLORS
+        self.save_plate_image = save_plate_image
+        if save_plate_image:
+            vid_name = os.path.basename(self.source).split('.')[0]
+            self.saved_path = "data/{vid_name}".format(vid_name=vid_name)
+            if os.path.exists(self.saved_path):
+                shutil.rmtree(self.saved_path)
+            os.makedirs(self.saved_path)
+
     def set_resolution(self, image):
+        """
+        Set video resolution for displaying only.
+        Note: this function doesn't config the input video of the model.
+        Arg:
+            image (OpenCV image): video frame
+        """
         height, width, _ = image.shape
         ratio = height / width
         if self.use_sd_resolution:
@@ -76,55 +128,74 @@ class Pipeline():
         return image
 
     def ocr(self, image):
+        """
+        Run OCR on the detected license plate
+        """
         text = self.ocr_model(image)
         return text
 
     def run(self):
+        """
+        Run the pipeline end2end
+        """
         cap = cv2.VideoCapture(self.source)
-        cap.set(cv2.CAP_PROP_FPS, int(30))
-        if (cap.isOpened()== False):
-            print("Error opening video file")
+        cap.set(cv2.CAP_PROP_FPS, 5)
+        count = 0 # for counting detected plates
         while(cap.isOpened()):
             ret, frame = cap.read()
-            if ret == True:
-                vehicle_results = self.vehicle_model(source=frame, save=False, conf=0.5)
+            if ret:
+                vehicle_results = self.vehicle_model(source=frame, save=False, conf=0.5, verbose=0)
                 vehicle_boxes = vehicle_results[0].boxes.xyxy
-
-                bgr_blue = (255, 0, 0)
-                bgr_green = (0, 255, 0)
-                bgr_red = (0, 0, 255)
-                bgr_amber = (0, 191, 255)
-
+                detected_plates = []
                 for box in vehicle_boxes:
                     if box is None:
                         continue
                     box = box.cpu().numpy().astype(int)
-                    cv2.rectangle(frame, (box[0], box[1]), (box[2], box[3]), bgr_green, thickness=2)
-
-                    im = frame[box[1]:box[3], box[0]:box[2], :] # crop a half of a vehicle
-                    plate_results = self.plate_model(source=im, save=False, conf=0.1)
+                    cv2.rectangle(frame, (box[0], box[1]), (box[2], box[3]), self.color["green"], thickness=2)
+                    cropped_vehicle = frame[box[1]:box[3], box[0]:box[2], :] # crop a vehicle
+                    plate_results = self.plate_model(source=cropped_vehicle, save=False, conf=0.1, verbose=0)
                     plate_boxes = plate_results[0].boxes.xyxy
 
+                    # Displaying plate detection
                     for plate_box in plate_boxes:
-                        if plate_box is None:
-                            break
                         plate_box = plate_box.cpu().numpy().astype(int)
                         src_point = (plate_box[0]+box[0], plate_box[1]+box[1])
                         dst_point = (plate_box[2]+box[0], plate_box[3]+box[1])
-                        cv2.rectangle(frame, src_point, dst_point, bgr_red, thickness=2)
+                        cv2.rectangle(frame, src_point, dst_point, self.color["red"], thickness=2)
+                        cropped_plate = cropped_vehicle[plate_box[1]:plate_box[3], plate_box[0]:plate_box[2], :]
+                        detected_plates.append(cropped_plate)
+                        if self.save_plate_image and check_image_size(cropped_plate, 64, 64):
+                            detected_plates.append(cropped_plate)
+                            filename = os.path.join(self.saved_path, str(count)+".jpg")
+                            cv2.imwrite(filename, cropped_plate)
+                            count += 1
 
-                    ocr_text = self.ocr(im)
-                    pos = (box[0], box[1]-5)
-                    draw_text(frame, ocr_text, pos)
-                
+                    # OCR the detected plate and display to monitor
+                    if len(detected_plates) > 0:
+                        cropped_vehicle = torch.from_numpy(cropped_vehicle)
+                        ocr_text = self.ocr(cropped_vehicle)
+                        pos = (box[0], box[1]-5)
+                        draw_text(img = frame,
+                                text = ocr_text,
+                                pos = pos,
+                                text_color=self.color["red"])
+
+                # Display to monitor
+                num_plate_info = "Detected plates: " + str(len(detected_plates))
+                draw_text(img = frame,
+                              text = num_plate_info,
+                              pos = (0, 10),
+                              font_scale=2,
+                              text_color=self.color["red"])
                 frame = self.set_resolution(frame)
                 cv2.imshow('Frame', frame)
                 if cv2.waitKey(25) & 0xFF == ord('q'):
                     break
-            
+
 if __name__ == "__main__":
     pipeline = Pipeline(source="data/TuKy.mp4",
                         vehicle_weight="weights/vehicle_yolov8.pt",
                         plate_weight="weights/plate_yolov8.pt",
-                        use_hd_resolution=True)
+                        use_sd_resolution=True,
+                        save_plate_image=True)
     pipeline.run()
