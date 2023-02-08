3dc01bed54ee8e835a31e0fca71b97722542eeab
tungedng2710 <tungnguyen99.tn@gmail.com>
2023-02-08 14:39:11 +0700

    minnor update

--- train_yolov8.py
+++ train_yolov8.py
@@ -1,6 +1,10 @@
+"""
+Train the YOLOv8 model
+"""
 from ultralytics import YOLO
 
 model = YOLO("yolov8n.yaml")
-results = model.train(data="data/plate_yolov8/data.yaml", 
-                      epochs=150,
-                      batch=32)
+model.train(data="data/plate_yolov8/data.yaml",
+            epochs=150,
+            batch=32,
+            device=[0])
