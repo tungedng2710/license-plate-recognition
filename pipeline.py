6e67205e97310157b6eb84cfb293e5d8ebba92bf
datnt114 <datnt114@viettel.com.vn>
2023-02-15 10:09:23 +0700

    add ppocr

--- pipeline.py
+++ pipeline.py
@@ -10,16 +10,17 @@ import torch
 from ultralytics import YOLO
 from tracking.utils.parser import get_config
 from tracking.deep_sort import DeepSort
-from utils.ocr import DummyOCR, EasyOCR
+from utils.ocr import PPOCR
 from utils.utils import map_label, check_image_size, draw_text, resize_, draw_box, \
     draw_tracked_boxes, compute_color, set_hd_resolution, BGR_COLORS, VEHICLES
+
 
 def get_args():
     """
     Get parsing arguments
     """
     parser = argparse.ArgumentParser()
-    parser.add_argument("--source", type=str, default="data/cam1.mp4", help="path to video, 0 for webcam")
+    parser.add_argument("--source", type=str, default="cam1.mp4", help="path to video, 0 for webcam")
     parser.add_argument("--vehicle_weight", type=str,
                         default="weights/vehicle_yolov8n.pt",
                         help="path to the yolov8 weight of vehicle detector")
@@ -32,6 +33,7 @@ def get_args():
     parser.add_argument("--config_deepsort", type=str, default="tracking/configs/deep_sort.yaml")
     return parser.parse_args()
 
+
 class Pipeline():
     """
     License plate OCR pipeline
@@ -41,6 +43,7 @@ class Pipeline():
     - plate_weight (str): path to the yolov8 weight of plate detector
     - save_result (bool): save cropped plate and output video to file
     """
+
     def __init__(self,
                  source: str = "data",
                  vehicle_weight: str = None,
@@ -51,17 +54,19 @@ class Pipeline():
         self.vehicle_weight = vehicle_weight
         self.vehicle_model = YOLO(vehicle_weight)
         self.plate_model = YOLO(plate_weight)
-        self.ocr_model = EasyOCR()
+        self.ocr_model = PPOCR()
 
         # DeepSort
         self.config_deepsort = config_deepsort
         cfg = get_config()
         cfg.merge_from_file(self.config_deepsort)
         self.deepsort = DeepSort(cfg.DEEPSORT.REID_CKPT,
-                        max_dist=cfg.DEEPSORT.MAX_DIST, min_confidence=cfg.DEEPSORT.MIN_CONFIDENCE,
-                        nms_max_overlap=cfg.DEEPSORT.NMS_MAX_OVERLAP, max_iou_distance=cfg.DEEPSORT.MAX_IOU_DISTANCE,
-                        max_age=cfg.DEEPSORT.MAX_AGE, n_init=cfg.DEEPSORT.N_INIT, nn_budget=cfg.DEEPSORT.NN_BUDGET,
-                        use_cuda=True)
+                                 max_dist=cfg.DEEPSORT.MAX_DIST, min_confidence=cfg.DEEPSORT.MIN_CONFIDENCE,
+                                 nms_max_overlap=cfg.DEEPSORT.NMS_MAX_OVERLAP,
+                                 max_iou_distance=cfg.DEEPSORT.MAX_IOU_DISTANCE,
+                                 max_age=cfg.DEEPSORT.MAX_AGE, n_init=cfg.DEEPSORT.N_INIT,
+                                 nn_budget=cfg.DEEPSORT.NN_BUDGET,
+                                 use_cuda=True)
 
         # Miscellaneous for displaying
         self.color = BGR_COLORS
@@ -95,9 +100,9 @@ class Pipeline():
             if os.path.exists(saved_plate_path):
                 shutil.rmtree(saved_plate_path)
             os.makedirs(saved_plate_path)
-        
+
         # -------------------------- MAIN --------------------------
-        count = 0 # for counting total detected plates from whole video
+        count = 0  # for counting total detected plates from whole video
         while cap.isOpened():
             ret, frame = cap.read()
             if frame is not None:
@@ -107,8 +112,8 @@ class Pipeline():
             if ret:
                 # Detect vehicles
                 vehicle_results = self.vehicle_model(source=frame, conf=vconf, verbose=False)
-                vehicle_boxes = vehicle_results[0].boxes.xyxy # Bounding box
-                vehicle_labels = vehicle_results[0].boxes.cls # Predicted classes
+                vehicle_boxes = vehicle_results[0].boxes.xyxy  # Bounding box
+                vehicle_labels = vehicle_results[0].boxes.cls  # Predicted classes
                 detected_plates = []
 
                 # ---------------------- Tracking ----------------------
@@ -129,8 +134,8 @@ class Pipeline():
                         continue
                     box = box.cpu().numpy().astype(int)
                     # Draw object information
-                    draw_text(img = displayed_frame, text = label_name, 
-                              pos = (int((box[0]+box[2])/2), box[1]),
+                    draw_text(img=displayed_frame, text=label_name,
+                              pos=(int((box[0] + box[2]) / 2), box[1]),
                               text_color=self.color["blue"],
                               text_color_bg=self.color["green"])
 
@@ -143,28 +148,29 @@ class Pipeline():
                     # Displaying plate detection
                     for plate_box in plate_boxes:
                         plate_box = plate_box.cpu().numpy().astype(int)
-                        src_point = (plate_box[0]+focused_box[0], plate_box[1]+focused_box[1])
-                        dst_point = (plate_box[2]+focused_box[0], plate_box[3]+focused_box[1])
+                        src_point = (plate_box[0] + focused_box[0], plate_box[1] + focused_box[1])
+                        dst_point = (plate_box[2] + focused_box[0], plate_box[3] + focused_box[1])
                         cv2.rectangle(displayed_frame, src_point, dst_point, self.color["red"], thickness=2)
                         cropped_plate = cropped_vehicle[plate_box[1]:plate_box[3], plate_box[0]:plate_box[2], :]
 
                         # Zoom in and display the plate
-                        displayed_plate = resize_(cropped_plate, 3)
-                        plate_pos = (int(displayed_plate.shape[0]) + box[1],
-                                     int(displayed_plate.shape[1]) + box[0])
-                        adjust_height = int((box[3]-box[1]) / 2)
                         try:
-                            displayed_frame[box[1]+adjust_height:plate_pos[0]+adjust_height, \
-                                            box[0]:plate_pos[1], :] = displayed_plate
+
+                            displayed_plate = resize_(cropped_plate, 3)
+                            plate_pos = (int(displayed_plate.shape[0]) + box[1],
+                                         int(displayed_plate.shape[1]) + box[0])
+                            adjust_height = int((box[3] - box[1]) / 2)
+                            displayed_frame[box[1] + adjust_height:plate_pos[0] + adjust_height, \
+                            box[0]:plate_pos[1], :] = displayed_plate
                         except:
                             pass
 
-                        if check_image_size(cropped_plate, 8, 8): # Ignore plates smaller than 8x8
+                        if check_image_size(cropped_plate, 8, 8):  # Ignore plates smaller than 8x8
                             have_plate = True
                             detected_plates.append(cropped_plate)
                             # Save the cropped plate, ignore if its size smaller than 32x32
                             if save_result and check_image_size(cropped_plate, 32, 32):
-                                filename = os.path.join(saved_plate_path, str(count)+".jpg")
+                                filename = os.path.join(saved_plate_path, str(count) + ".jpg")
                                 cv2.imwrite(filename, cropped_plate)
                                 count += 1
 
@@ -175,15 +181,15 @@ class Pipeline():
                             ocr_text = self.ocr(cropped_plate)
                             # ----------------------------------------
                             # Display to monitor
-                            pos = (int((box[0]+box[2])/2), box[1])
+                            pos = (int((box[0] + box[2]) / 2), box[1])
                             info = f"{label_name} {ocr_text}"
-                            draw_text(img = displayed_frame, text = info, pos = pos,
+                            draw_text(img=displayed_frame, text=info, pos=pos,
                                       text_color=self.color["blue"],
                                       text_color_bg=self.color["green"])
 
                 # Display global detection info
                 num_plate_info = "Detected plates: " + str(len(detected_plates))
-                draw_text(img = displayed_frame, text = num_plate_info, pos = (0, 0), font_scale=1,
+                draw_text(img=displayed_frame, text=num_plate_info, pos=(0, 0), font_scale=1,
                           text_color=self.color["black"],
                           text_color_bg=self.color["amber"])
                 if save_result:
@@ -202,13 +208,15 @@ class Pipeline():
             vid_writer.release()
         cv2.destroyAllWindows()
 
+
 if __name__ == "__main__":
     args = get_args()
+    print(args)
     pipeline = Pipeline(source=args.source,
                         vehicle_weight=args.vehicle_weight,
                         plate_weight=args.plate_weight,
                         config_deepsort=args.config_deepsort)
     pipeline.run(vconf=args.vconf,
                  pconf=args.pconf,
-                 hd_resolution = True,
+                 hd_resolution=True,
                  save_result=args.save)
