cba7034aeb231c66de2428cf6f517a2a15ab6a40
Tung Nguyen <tungnguyen99.tn@gmail.com>
2025-06-30 09:50:35 +0700

    update onnx inference

--- detectors/yolov8/ultralytic.py
+++ detectors/yolov8/ultralytic.py
@@ -24,11 +24,11 @@ class YOLOv8():
     
     def export(self):
         self.model.info(verbose=True)
-        self.model.export(format="onnx", dynamic=False, opset=12, batch=1)
+        self.model.export(format="onnx", dynamic=True, opset=12, imgsz=320)
         
 
 if __name__ == "__main__":
-    yolov8 = YOLOv8("license-plate-recognition/vehicle_yolov8s_640.pt")
+    yolov8 = YOLOv8("weights/plate_yolov8n_320_2024.pt")
     yolov8.export()
 
     # image = np.random.rand(1, 3, 640, 640).astype(np.float32)
